28304daf2deb00b0219a14ec25a92a86bf9a6d5e
sourav <hatisourav17@gmail.com>
2024-04-29 22:42:05 +0530

    Home page Hero 1 UI done

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,14 +6,29 @@ import '../../styles/HeaderStyles.css'
 import {Link} from 'react-router-dom'
 import Logo from '../../assets/logo/logo_3.2.png'
 export default function Header() {
+
+    const [nav, setNav] = useState(false);
+    const white = require('../../assets/logo/logo_3.2.png')
+    const black = require('../../assets/logo/logo_3.png')
+  
+    
+    // Scroll Navbar
+    const changeValueOnScroll = () => {
+      const scrollValue = document?.documentElement?.scrollTop;
+      scrollValue > 100 ? setNav(true) : setNav(false);
+    };
+  
+    window.addEventListener("scroll", changeValueOnScroll);
+  
     return (
+
         <>
         <header>
-    <Navbar  collapseOnSelect expand="lg">
+    <Navbar  collapseOnSelect expand="lg"  className={`${nav === true ? "sticky" : ""}`}>
     <Container>
       <Navbar.Brand href="#home">
         <Link className='logo'>
-        <img src={Logo} alt="logo" className="img-fluid" />
+        <img src={`${nav === true ? black : white}`} alt="logo" className="img-fluid" />
         </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
--- src/pages/Home/Section.js
+++ src/pages/Home/Section.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import Burger from '../../assets/hero/hero-2.png'
 import Biriyani from '../../assets/hero/biriyani.png'
+import plus from '../../assets/hero/plus_icon.jpg'
+import Drinks from '../../assets/hero/drinks.png'
+
 import { Link } from 'react-router-dom'
 export default function Section() {
     return (
@@ -11,6 +14,9 @@ export default function Section() {
                     <Col lg={7} className='mb-5 mb-lg-0'>
                         <div className="position-relative">
                             <img src={Biriyani} alt="hero" className='img-fluid' />
+                            {/* <img src={plus} alt="hero" className='img-fluid' /> */}
+                            <i class="bi bi-plus "  id='plus_icon' ></i>
+                            <img src={Drinks} alt="hero" className='img-fluid'  id ='drinks_img'/>
                             <div className="price_badge">
                                 <div className="badge_text">
                                     <h4 className="h4_xs">Only</h4>
@@ -21,7 +27,7 @@ export default function Section() {
                         </Col>
                         <Col lg={5}>
                             <div className="hero_text text-center">
-                                <h1 className="text-white">Biriyani </h1>
+                                <h1 className="text-white"> Special Biriyani </h1>
                                 <h2 className="text-white">With Free Coldrinks</h2>
                                 <p className="text-white pt-2 pb-4">
                                     Feugiat primis ligula risus auctor laoreet augue egestas mauris
